99f718b05178a9a8236490bdb8d89faea4e53289
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-08-07 13:10:37 -0500

    getting started...basic outline

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,17 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+const basket = []; // the basket
+
+function addItem (item) {
+  return true; // indicates item was added to basket
+}
+
+fuction listItems (array) {
+  // loop to list each item on a new line
+}
+
+function empty(array) {
+  // resets basket to empty array
+}

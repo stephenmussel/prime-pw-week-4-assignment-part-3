789a906cc88d9d5e6afa153e8e5ce7ad613fdb2e
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-08-07 14:07:50 -0500

    able to list item on separate line, but getting additional output of undefined

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -8,11 +8,16 @@ function addItem (item) {
   basket.push(item) // puts the item in the basket
   return true; // indicates item was added to basket
 }
-/*
-fuction listItems (array) {
+
+function listItem (array) {
   // loop to list each item on a new line
+  for (let i = 0; i <basket.length; i++) {
+    console.log(basket[i]);
+  }
 }
 
+
+/*
 function empty(array) {
   // resets basket to empty array
 }
@@ -21,4 +26,9 @@ function empty(array) {
 console.log('***** TESTING function addItem *****'); // header
 console.log(`Basket has: ${basket}`); // initial contents of basket should be empty
 console.log(`Adding mozzarella (expect true), ${addItem('mozzarella')}`); // adding mozzarella and outputs true if added to basket
+console.log(`Adding salmon (expect true), ${addItem('salmon')}`); // adding salmon and outputs true if added to basket
+console.log(`Adding cucumber (expect true), ${addItem('cucumber')}`); // adding cucumber and outputs true if added to basket
 console.log(`Basket has: ${basket}`); // shows basket with mozzarella
+
+console.log('***** TESTING function listItem *****'); // header
+console.log(listItem());

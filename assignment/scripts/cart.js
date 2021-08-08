86dd95f4168b4650e25dc182a4db86a29031d815
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-08-08 11:07:22 -0500

    finished most of stretch #4. need to return removed item

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,7 +3,7 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 const basket = []; // the basket
-const maxItems = 5;
+const maxItems = 5; // when basket is full
 
 function addItem (item) {
   basket.push(item) // puts the item in the basket
@@ -12,7 +12,7 @@ function addItem (item) {
   }
   else {
     console.log('Basket is full!');
-    while (basket.length > 5){
+    while (basket.length > 5){ // removes item if basket has more than 5 items
       basket.pop();
     }
     return false;
@@ -26,7 +26,7 @@ function listItem (array) {
 }
 
 function empty(array) {
-  while (basket.length > 0) {
+  while (basket.length > 0) { // loops list to remove last item until basket empty
     basket.pop();
   }
 }
@@ -40,23 +40,33 @@ function isFull() {
   }
 }
 
-console.log('***** TESTING function addItem *****'); // header
+function removeItem (item) {
+  // return the item removed or null if the item was not found
+  let itemRemoved = basket.indexOf(item); // this is the item index of what you're looking for
+  if (itemRemoved > -1) { // if the item/index is found, then remove it. -1 means element does not exist in array
+    basket.splice (itemRemoved, 1); // removes element. starts at itemRemoved location and removes one element
+    return basket;
+  }
+  return null; // item not found
+}
+
+console.log('***** TESTING function addItem *****');
 console.log(`Basket has: ${basket}`); // initial contents of basket should be empty
 console.log(`Adding mozzarella (expect true), ${addItem('mozzarella')}`); // adding mozzarella and outputs true if added to basket
 console.log(`Adding salmon (expect true), ${addItem('salmon')}`); // adding salmon and outputs true if added to basket
 console.log(`Adding cucumber (expect true), ${addItem('cucumber')}`); // adding cucumber and outputs true if added to basket
 console.log(`Basket has: ${basket}`); // shows basket with mozzarella
 
-console.log('***** TESTING function listItem *****'); // header
+console.log('***** TESTING function listItem *****');
 console.log(listItem()); // lists items on separate line but outputs another line as undefined!?
 
-console.log('***** TESTING function empty *****'); // header
+console.log('***** TESTING function empty *****');
 console.log(empty()); // basket empty
 
-console.log('***** TESTING function isFull *****'); // header
+console.log('***** TESTING function isFull *****');
 console.log(isFull()); // basket is not full
 
-console.log('***** TESTING function isFull and maxItems *****'); // header
+console.log('***** TESTING function isFull and maxItems *****');
 console.log(listItem()); // lists items in basket
 console.log(`This is what's in the basket now: ${basket}`); // basket is empty
 
@@ -69,3 +79,8 @@ addItem('chips'); // add to basket.
 
 console.log(`Now the basket has: ${basket}`); // shows basket with fruit and more stuff
 console.log(`Is the basket full?: ${isFull()}`); // shows basket with fruit and more stuff
+
+console.log('***** TESTING function removeItem *****');
+console.log(`This is what's in the basket now: ${basket}`); // checking to see what's in the basket. it's full
+console.log(`This what's in the basket now: ${basket}. If there are peaches, remove them: ${removeItem('peaches')}.`); // removing existing item from basket
+console.log(`This what's in the basket now: ${basket}. If there are apples, remove them: ${removeItem('apples')}.`); // item not found
